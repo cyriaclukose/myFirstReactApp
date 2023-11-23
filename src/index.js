fb2309c0b5972ea6ada9cdff45efe7911d2e9aba
cyriac lukose <cyriaclukosekuruvila@gmail.com>
2023-11-23 23:38:01 +0000

    commit my react app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import  ReactDOM  from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const myfirstelement=<h1>hello world. This is my first react element.</h1>;
+// we have to create the root
+
+const myArray=['orange','banana','appl']; 
+      
+const mylist=myArray.map(item=><p>{item}</p>)
+
+const root =ReactDOM.createRoot(document.getElementById("root"))
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+<React.StrictMode>
+  {mylist}
+  </React.StrictMode>
+
+)
